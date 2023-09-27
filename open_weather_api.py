c9602058e383b8d0359b9d5015aa2dc41adc1d1a
Albaito <stanfordtramaine44@gmail.com>
2023-09-27 17:09:31 -0500

    added basiv logging

--- open_weather_api.py
+++ open_weather_api.py
@@ -2,19 +2,20 @@ import requests
 import logging
 import os
 
+logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 
 def main():
 
-    key = os.environ.get('WEATHER_KEY')
+    key = os.environ.get('WATHER_KEY')
     
     if key is None:
-        logging.warning('')
+        logging.warning('Environment Variable "WEATHER_KEY" not found')
         return
     
     city_country = input('What location would you like a 5-day forecast for: ').lower()
 
     if city_country is None:
-        print()
+        print('Error - Please enter a city in the following format "city,country code"')
         return
 
     query = {'q': city_country, 'units': 'imperial', 'appid': key}
@@ -29,11 +30,15 @@ def main():
     for forecast in forecast_items:
         date = forecast['dt_txt']   # Using city timezone as why would someone in X city want to know what time it is in UTC.
         temp = forecast['main']['temp']
+        wind_speed = forecast['wind']['speed']
         weather = forecast['weather'][0]['description']
-        print(f'at {date} local time, it will be {weather} with a temperature of {temp}F in {city_name}')
+        print(f'at {date} local time, it will be {weather} with a temperature of {temp}F in {city_name}, The wind speed is {wind_speed} miles per hour.')
+        logging.debug(f'DATE: {date}, CITY: {city_name}')
 
 if __name__ == '__main__':
+    logging.info('5 Day Forecast Program Launched')
     main()
+
 
 '''
 Part 2
